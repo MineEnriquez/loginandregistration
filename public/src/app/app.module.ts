973616d253c168affe5876c0ad1a83baa8c9ab61
MineEnriquez <38847090+MineEnriquez@users.noreply.github.com>
2019-11-08 20:03:45 -0800

    Step 2 - Register the service
    - go to app.module.ts
    - add the import
    - register the provider

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpService } from "./http.service";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { importExpr } from '@angular/compiler/src/output/output_ast';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [HttpService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
